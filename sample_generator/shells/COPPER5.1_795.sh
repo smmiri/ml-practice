65b27ad25bde1839bda523624bcd313bdc323c6c
Mohammad Miri <smohammad.miri@gmail.com>
2022-10-07 10:49:56 -0700

    further cleanup

--- sample_generator/shells/COPPER5.1_795.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --output=COPPER5_795.out 
-
-export PATH=$PATH:/home/smmiri/CPLEX/cplex/bin/x86-64_linux/   
-export PATH=$PATH:/home/smmiri/CPLEX/cplex/python/3.7/x86-64_linux/  
-
-source /home/smmiri/copper-env/bin/activate
-python /scratch/smmiri/COPPER5.1_795.py 
--- sample_generator/shells/COPPER5.1_961.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --output=COPPER5_961.out 
-
-export PATH=$PATH:/home/smmiri/CPLEX/cplex/bin/x86-64_linux/   
-export PATH=$PATH:/home/smmiri/CPLEX/cplex/python/3.7/x86-64_linux/  
-
-source /home/smmiri/copper-env/bin/activate
-python /scratch/smmiri/COPPER5.1_961.py 
